fadcd89987c1110a93e69ca6359d070d637f4e90
Marc Roettig <marc.roettig@googlemail.com>
2011-09-07 21:00:33 +0200

    added some helper functions.

---
+++ src/org/roettig/NRPSpredictor2/util/Helper.java
@@ -0,0 +1,34 @@
+package org.roettig.NRPSpredictor2.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.roettig.NRPSpredictor2.hmmer.HMMPfam;
+
+public class Helper
+{
+	public static void deployFile(InputStream in, String filename) throws IOException
+	{
+		FileOutputStream fout = new FileOutputStream(filename);
+		byte[] buffer = new byte[2048];
+		int size;
+		
+		while ((size = in.read(buffer, 0, 2048)) != -1)
+		{
+			fout.write(buffer, 0, size);
+		}
+		fout.flush();
+		fout.close();
+		in.close();
+	}
+	
+	public static File deployFile(InputStream in) throws IOException
+	{
+		File tmp = File.createTempFile("TEMP", "TMP");
+		deployFile(in,tmp.getAbsolutePath());
+		return tmp;
+	}
+}
