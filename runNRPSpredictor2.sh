0bd13a9821502a62dda7cc34e48583e0f31db841
Marc Roettig <marc.roettig@googlemail.com>
2011-09-07 21:02:24 +0200

    added examplary shell script launching NRPSPredictor2.

---
+++ runNRPSpredictor2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Ddatadir=data -cp lib/dom4j-1.6.1.jar:lib/jaxen-1.1-beta-6.jar:build/NRPSpredictor2.jar:lib/java-getopt-1.0.13.jar:lib/Utilities.jar:lib/libsvm.jar org.roettig.NRPSpredictor2.NRPSpredictor2 $*
