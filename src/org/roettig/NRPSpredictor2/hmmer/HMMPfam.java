577af548a11dd00988c3584706b65fc05d5b0f8b
Marc Roettig <marc.roettig@googlemail.com>
2011-09-10 15:43:20 +0200

    added windows support for hmmpfam (tested on Win XP).

--- src/org/roettig/NRPSpredictor2/hmmer/HMMPfam.java
+++ src/org/roettig/NRPSpredictor2/hmmer/HMMPfam.java
@@ -1,51 +1,42 @@
 package org.roettig.NRPSpredictor2.hmmer;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.List;
-
-import org.roettig.NRPSpredictor2.hmmer.HMMPfamParser.DomainAlignment;
-import org.roettig.NRPSpredictor2.hmmer.HMMPfamParser.DomainHit;
 import org.roettig.NRPSpredictor2.hmmer.HMMPfamParser.QueryResult;
+import org.roettig.NRPSpredictor2.util.Helper;
 import org.roettig.NRPSpredictor2.util.ToolRunner;
 
 public class HMMPfam
 {
 	private static File executable;
+	private static String tmpdir;
+
 	static
 	{
 		// deploy excutable
 		try
 		{
 			executable = File.createTempFile("HMMPFAM", "EXE");
-			FileOutputStream fout = new FileOutputStream(executable);
-			byte[] buffer = new byte[2048];
-			int size;
-			
-			InputStream zin = null;
 			
 			String os = System.getProperty("os.name");
-			
+			tmpdir    = System.getProperty("java.io.tmpdir");
+
 			if(os.toLowerCase().contains("nux")||os.toLowerCase().contains("nix"))
 			{
-				zin = HMMPfam.class.getResourceAsStream("hmmpfam");
+				executable = new File(tmpdir+File.separator+"hmmpfam");
+				Helper.deployFile(HMMPfam.class.getResourceAsStream("hmmpfam"), executable.getAbsolutePath());
 			}
-			if(os.toLowerCase().contains("mac"))
+			else if(os.toLowerCase().contains("mac"))
 			{
-				zin = HMMPfam.class.getResourceAsStream("hmmpfam.macos64");
+				executable = new File(tmpdir+File.separator+"hmmpfam");
+				Helper.deployFile(HMMPfam.class.getResourceAsStream("hmmpfam.macos64"), executable.getAbsolutePath());
 			}
-			
-			if(zin==null)
-				throw new Exception("could not deploy hmmpfam binary for your platform :"+os);
-			
-			while ((size = zin.read(buffer, 0, 2048)) != -1)
+			else if(os.toLowerCase().contains("win"))
 			{
-				fout.write(buffer, 0, size);
-			}
-			fout.flush();
-			fout.close();
-			zin.close();
+				executable = new File(tmpdir+File.separator+"hmmpfam.exe");
+				Helper.deployFile(HMMPfam.class.getResourceAsStream("hmmpfam.win32"), executable.getAbsolutePath());
+			 	Helper.deployFile(HMMPfam.class.getResourceAsStream("cygwin1.dll"), tmpdir+File.separator+"cygwin1.dll");
+			} 
 			executable.setExecutable(true);
 			executable.deleteOnExit();
 		} 
@@ -64,14 +55,17 @@ public class HMMPfam
 	
 	public void run(double evalue, File hmmmodel, File sequences) throws Exception
 	{
-		logfile = File.createTempFile("HMMPFAM", "");
+		logfile = File.createTempFile("HMMPFAM", "RESULT");
 		logfile.deleteOnExit();
 
 		ToolRunner tr = new ToolRunner();
+		tr.setJobDir(tmpdir);
 		tr.run(executable.getAbsolutePath(), "-E", String.valueOf(evalue), "-l", logfile.getAbsolutePath(), hmmmodel.getAbsolutePath(), sequences.getAbsolutePath());
-		
 		if(tr.getReturnCode()!=0)
+		{
+			System.err.println(tr.getOutput());
 			throw new Exception("hmmpfam terminated with non-zero exit code");
+		}
 	}
 	
 	public List<QueryResult> getResults() throws Exception
@@ -79,24 +73,4 @@ public class HMMPfam
 		HMMPfamParser parser = new HMMPfamParser(logfile.getAbsolutePath());
 		return parser.getResultsForQueries();
 	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		HMMPfam pfam = new HMMPfam();
-		pfam.run( 0.001, new File("/tmp/aa-activating.aroundLys.hmm"), new File("/tmp/in.fa"));
-		List<QueryResult> res = pfam.getResults();
-		for(QueryResult q: res)
-		{
-			System.out.println(q.getQueryId());
-			for(DomainHit hit: q.getHits())
-			{
-				System.out.println(hit.toString());
-			}
-			for(DomainAlignment ali: q.getAlignments())
-			{
-				System.out.println(ali.toString());
-			}
-		}
-	}
-
 }
