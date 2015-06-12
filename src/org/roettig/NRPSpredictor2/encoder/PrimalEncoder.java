8222dcf8b02623fd9748d16640d51d927e344166
marc.roettig <roettig@roettig-K55VD>
2015-06-12 21:18:23 +0200

    Added documentation.

--- src/org/roettig/NRPSpredictor2/encoder/PrimalEncoder.java
+++ src/org/roettig/NRPSpredictor2/encoder/PrimalEncoder.java
@@ -1,6 +1,20 @@
 package org.roettig.NRPSpredictor2.encoder;
 
+/**
+ * This interface gives a feature encoding method
+ * for strings (=sequence of characters).
+ * 
+ * @author roettig
+ *
+ */
 public interface PrimalEncoder
 {
+	/**
+	 * Encode given string as n-dimensional feature vector.
+	 * 
+	 * @param s the string to encode(character-wise)
+	 * 
+	 * @return feature vector (as double array)
+	 */
 	double[] encode(String s);
 }
