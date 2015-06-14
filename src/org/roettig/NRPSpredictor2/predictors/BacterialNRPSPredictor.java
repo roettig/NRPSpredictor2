9bf2f7606e50c8cf5e7ba13acef2950a3178d48c
marc.roettig <roettig@roettig-K55VD>
2015-06-14 23:02:48 +0200

    cleanup.

--- src/org/roettig/NRPSpredictor2/predictors/BacterialNRPSPredictor.java
+++ src/org/roettig/NRPSpredictor2/predictors/BacterialNRPSPredictor.java
@@ -3,24 +3,57 @@ package org.roettig.NRPSpredictor2.predictors;
 import org.roettig.NRPSpredictor2.encoder.PrimalRauschEncoder;
 import org.roettig.NRPSpredictor2.extraction.ADomain;
 
-public class BacterialNRPSPredictor extends PredictorBase
+public class BacterialNRPSPredictor 
+extends 
+	PredictorBase
 {
-	// large cluster predictions
-	private static String large_cluster[] = { "phe,trp,phg,tyr,bht", "ser,thr,dhpg,hpg",
+	/**
+	 * Large cluster prediction labels.
+	 */
+	public static String LARGE_CLUSTER[] = { "phe,trp,phg,tyr,bht", "ser,thr,dhpg,hpg",
 			"gly,ala,val,leu,ile,abu,iva", "asp,asn,glu,gln,aad", "cys",
 			"orn,lys,arg", "pro,pip", "dhb,sal" };
 
-	// small cluster predictions
-	private static String small_cluster[] = { "aad", "val,leu,ile,abu,iva", "arg", "asp,asn",
+	public static int LARGE_CLUSTER_PHE_TRP_PHG_TYR_BHT         = 0;
+	public static int LARGE_CLUSTER_SER_THR_DHPG_HPG            = 1;
+	public static int LARGE_CLUSTER_GLY_ALA_VAL_LEU_ILE_ABU_IVA = 2;
+	public static int LARGE_CLUSTER_ASP_ASN_GLU_GLN_AAD         = 3;
+	public static int LARGE_CLUSTER_CYS                         = 4;
+	public static int LARGE_CLUSTER_ORN_LYS_ARG                 = 5;
+	public static int LARGE_CLUSTER_PRO_PIP                     = 6;
+	public static int LARGE_CLUSTER_DHB_SAL                     = 7;
+	
+	
+	/**
+	 *  Small cluster prediction labels.
+	 */
+	public static String SMALL_CLUSTER[] = { "aad", "val,leu,ile,abu,iva", "arg", "asp,asn",
 			"cys", "dhb,sal", "glu,gln", "orn,horn", "tyr,bht", "pro", "ser",
 			"dhpg,hpg", "phe,trp", "gly,ala", "thr" };
 
+	public static int SMALL_CLUSTER_AAD                 = 0;
+	public static int SMALL_CLUSTER_VAL_LEU_ILE_ABU_IVA = 1;
+	public static int SMALL_CLUSTER_ARG                 = 2;
+	public static int SMALL_CLUSTER_ASP_ASN             = 3;
+	public static int SMALL_CLUSTER_CYS                 = 4;
+	public static int SMALL_CLUSTER_DHB_SAL             = 5;
+	public static int SMALL_CLUSTER_GLU_GLN             = 6;
+	public static int SMALL_CLUSTER_ORN                 = 7;
+	public static int SMALL_CLUSTER_TYR_BHT             = 8;
+	public static int SMALL_CLUSTER_PRO                 = 9;
+	public static int SMALL_CLUSTER_SER                 = 10;
+	public static int SMALL_CLUSTER_DHPG_HPG            = 11;
+	public static int SMALL_CLUSTER_PHE_TRP             = 12;
+	public static int SMALL_CLUSTER_GLY_ALA             = 13;
+	public static int SMALL_CLUSTER_THR                 = 14;
+	
+	
 	public BacterialNRPSPredictor()
 	{
 		encoder = new PrimalRauschEncoder();
 		
 		String type = ADomain.NRPS1_LARGE_CLUSTER;
-		for (String label : large_cluster)
+		for (String label : LARGE_CLUSTER)
 		{
 			NRPSPredictorModel model = NRPSPredictorModel.loadModel(label, type);
 			model.setLabel(label);
@@ -28,7 +61,7 @@ public class BacterialNRPSPredictor extends PredictorBase
 			models.add(model);
 		}
 		type = ADomain.NRPS1_SMALL_CLUSTER;
-		for (String label : small_cluster)
+		for (String label : SMALL_CLUSTER)
 		{
 			NRPSPredictorModel model = NRPSPredictorModel.loadModel(label, type);
 			model.setLabel(label);
--- srctest/org/roettig/NRPSpredictor2/NRPSpredictor2Test.java
+++ srctest/org/roettig/NRPSpredictor2/NRPSpredictor2Test.java
@@ -1,18 +1,56 @@
 package org.roettig.NRPSpredictor2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.roettig.NRPSpredictor2.extraction.ADomain;
+import org.roettig.NRPSpredictor2.predictors.BacterialNRPSPredictor;
 import org.roettig.NRPSpredictor2.predictors.BacterialNRPSPredictor2;
 import org.roettig.NRPSpredictor2.predictors.Detection;
 
 public class NRPSpredictor2Test 
 {
-	public void testPredictorGeneric(
+	public void testNRPSpredictor1Generic(
+			String signature8A, 
+			String singleClassLabel,
+			String threeClassLabel
+			)
+	{
+		List<ADomain> adoms = new ArrayList<ADomain>();
+		
+		ADomain adom = new ADomain(); 
+		adom.sig8a   = signature8A;
+		
+		adoms.add(adom);
+		
+		BacterialNRPSPredictor bactPred = new BacterialNRPSPredictor();
+		bactPred.predict(adoms);
+		
+		boolean hasSinglePrediction     = false;
+		boolean hasThreeClassPrediction = false;
+		
+		for(Detection det: adom.getDetections())
+		{
+			if(det.getLabel().equalsIgnoreCase(singleClassLabel))
+			{
+				hasSinglePrediction = true;
+			}
+			if( det.getLabel().equalsIgnoreCase(threeClassLabel))
+			{
+				hasThreeClassPrediction = true;
+			}
+		}
+		
+		assertTrue(hasSinglePrediction);
+		if(null!=threeClassLabel)
+			assertTrue(hasThreeClassPrediction);
+	}
+	
+	
+	public void testNRPSpredictor2Generic(
 			String signature8A, 
 			String singleClassLabel,
 			String threeClassLabel
@@ -49,186 +87,206 @@ public class NRPSpredictor2Test
 	}
 	
 	@Test
-	public void testPredictor()
+	public void testPredictorNRPSpredictor1()
+	{
+		testNRPSpredictor1Generic
+		(		
+				"ADLAFEPFMRQINGLLVGEINEYAFTETAFVTAI",
+				BacterialNRPSPredictor.SMALL_CLUSTER[BacterialNRPSPredictor.SMALL_CLUSTER_AAD],
+				BacterialNRPSPredictor.LARGE_CLUSTER[BacterialNRPSPredictor.LARGE_CLUSTER_ASP_ASN_GLU_GLN_AAD]
+		);
+		
+		testNRPSpredictor1Generic
+		(		
+				"LNNSFDASTLDAWMIVGGDLNGYGPTEATTFSAT",
+				BacterialNRPSPredictor.SMALL_CLUSTER[BacterialNRPSPredictor.SMALL_CLUSTER_VAL_LEU_ILE_ABU_IVA],
+				BacterialNRPSPredictor.LARGE_CLUSTER[BacterialNRPSPredictor.LARGE_CLUSTER_GLY_ALA_VAL_LEU_ILE_ABU_IVA]
+		);
+		
+		
+	}
+	
+	@Test
+	public void testPredictorNRPSpredictor2()
 	{
 		
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(		
 				"ADLAFEPFMRQINGLLVGEINEYAFTETAFVTAI",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_AAD],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"FWATFDLAVYEANTNVAGECNLYGPSETTTYSSW",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ALA],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"VAWAFDVFSADRDFVCGSDVQAYGVTEASIDSTC",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ARG],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"YWASFDLTVTSTKLIVGGEFNEYGPTETVVGCMI",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ASN],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"YGVSADLGHTLGKIMLGGEVNHYGPTETTVGILT",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ASP],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LAQAFDAAISEMTVVVAGEINGYGPTETTVCVTM",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_BHT],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"TDISFDLSVYDGNSMLSGDIAMGGATEASIWSNA",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_CYS],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"SRSIYAMSSPGGALQVGGAQQVFGMAEGLVNYTR",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_DHB],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LDAAFDPSLYAVHLGTGGDRNTYGPTETTLCATW",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_DHPG],
 				null//BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"HWMTFDASVWEAQLFCGGEHNLYGPTETCIDATF",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_GLN],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"RCMAFDVSVWEWHFFSGGKHNRYGPTETAINVTH",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_GLU],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"FAMTFDIAALEHQGLVGGEVNLYGPTETTIWSTA",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_GLY],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LDHAFDVSCYEVHLLTGGDRHLYGPTETTLCVTQ",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_HPG],
 				null//BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"VETSFDGFTFDGFVLFGGEIHVYGPTETTVFATF",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ILE],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"SAIAFDLSIGEAYLLLAGEINGWGPAETCVFSTL",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_IVA],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"NAISFDFSILEMYLFAGGEINGWGPTETTVVGSI",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_LEU],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"YDHWFDAAWQPADTALGGEFNCYGPTETTVEAVV",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_LYS],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"RWQAFDISINELYLWCGGEYHGYGPAETTIGVSH",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_ORN],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHILIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"VPLAFDAALWELTLVVAGETNAYGPTEAAVCTTI",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_PHE],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LWQVFDYSVQESYACQGGEHNHYGPAESQLITGY",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_PIP],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LWHAFDVAAQESYAAQAGEHNHYGPAETHVMTGI",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_PRO],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"RWMTFDVSVWEWHFFCSGEHNLYGPTEAAVDVTY",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_SER],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LATHFDFSVWEGNQVFGGEVNMYGITETTVHVSH",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_THR],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_ALIPHATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LDRVFDVSMADPVMVSGGDHNEYGVTEATVVSTV",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_TRP],
 				null //BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 		
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LAQAFDAAVSEMTVVVAGEINAYGPTETTVCASM",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_TYR],
 				BacterialNRPSPredictor2.THREE_CLASS[BacterialNRPSPredictor2.THREE_CLASS_HYDROPHOBIC_AROMATIC]
 		);
 
-		testPredictorGeneric
+		testNRPSpredictor2Generic
 		(
 				"LNNSFDASTLDAWMIVGGDLNGYGPTEATTFSAT",
 				BacterialNRPSPredictor2.SINGLE_CLASS[BacterialNRPSPredictor2.SINGLE_CLASS_VAL],
