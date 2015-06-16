8a0b85c16a2c5df56be285b59776527fdaba9afb
marc.roettig <roettig@roettig-K55VD>
2015-06-16 22:15:39 +0200

    cleanup.

--- src/org/roettig/NRPSpredictor2/predictors/NearestNeighborPredictor.java
+++ src/org/roettig/NRPSpredictor2/predictors/NearestNeighborPredictor.java
@@ -1,15 +1,26 @@
 package org.roettig.NRPSpredictor2.predictors;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.roettig.NRPSpredictor2.NRPSpredictor2;
 import org.roettig.NRPSpredictor2.extraction.ADomain;
 
 public class NearestNeighborPredictor implements Predictor
 {
+	/**
+	 * The logger.
+	 */
+	private static final Logger logger = LogManager.getLogger(NRPSpredictor2.class);
+	
+	private static final String DEFAULT_ASCII_CODEPAGE = "CP1252";
+	
 	private List<String> sigstach        = new ArrayList<String>();
 	private List<String> sigstach_fungal = new ArrayList<String>();
 	private List<String> specs           = new ArrayList<String>();
@@ -26,7 +37,7 @@ public class NearestNeighborPredictor implements Predictor
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
+			logger.error(e);
 			throw new RuntimeException(e);
 		}
 	}
@@ -41,28 +52,45 @@ public class NearestNeighborPredictor implements Predictor
 		this.bacterialPrediction = bacterialPrediction;
 	}
 
-	private void initSigDB() throws IOException
+	private void initSigDB() 
+	throws 
+		IOException
 	{
-		BufferedReader in = new BufferedReader(new FileReader("data/labeled_sigs"));
-		String line = "";
-		while((line=in.readLine())!=null)
+		BufferedReader in = new BufferedReader(new InputStreamReader( new FileInputStream("data/labeled_sigs"), DEFAULT_ASCII_CODEPAGE));
+		try
 		{
-			String toks[] = line.split("\\\t");
-			specs.add(toks[0]);
-			sig8a.add(toks[1]);
-			sigstach.add(toks[2]);
+			String line = "";
+			while((line=in.readLine())!=null)
+			{
+				String toks[] = line.split("\\\t");
+				specs.add(toks[0]);
+				sig8a.add(toks[1]);
+				sigstach.add(toks[2]);
+			}
 		}
-		in.close();
+		finally
+		{
+			if(null!=in)
+				in.close();
+		}
 		
-		in = new BufferedReader(new FileReader("data/fungal_labeled_sigs"));
-		line = "";
-		while((line=in.readLine())!=null)
+		in = new BufferedReader(new InputStreamReader( new FileInputStream("data/fungal_labeled_sigs"), DEFAULT_ASCII_CODEPAGE));
+		
+		try
 		{
-			String toks[] = line.split("\\\t");
-			specs_fungal.add(toks[0]);
-			sigstach_fungal.add(toks[2]);
+			String line = "";
+			while((line=in.readLine())!=null)
+			{
+				String toks[] = line.split("\\\t");
+				specs_fungal.add(toks[0]);
+				sigstach_fungal.add(toks[2]);
+			}
 		}
-		in.close();
+		finally
+		{
+			if(null!=in)
+				in.close();
+		}
 	}
 	
 	@Override
@@ -83,7 +111,16 @@ public class NearestNeighborPredictor implements Predictor
 				ad.addDetection(ADomain.NRPS2_STACH_NN_FUNGAL, hitspec[0], sim);
 		}
 	}
-	
+
+	/**
+	 * Computes the nearest signature according to sequence identity of Stachelhaus code.
+	 * 
+	 * @param ad query ADomain
+	 * @param hit the best matching ADomain
+	 * @param bacterial consider only bacterial signatures
+	 * 
+	 * @return sequence identity distance
+	 */
 	public double nearestSignature(ADomain ad, String[] hit, boolean bacterial)
 	{
 		String a_sig   = ad.sigstach;
@@ -91,14 +128,24 @@ public class NearestNeighborPredictor implements Predictor
 		int idx = 0;
 		
 		List<String> sigs;
-		if(bacterial)
-			sigs = sigstach;
-		else
-			sigs = sigstach_fungal;
 		
+		if(bacterial)
+		{
+			// consider only bacterial signatures
+			sigs = sigstach;
+		}
+		else
+		{
+			// consider only fungal signatures
+			sigs = sigstach_fungal;
+		}
+		
+		// compute sequence identity to all candidates
 		for(String sig: sigs)
 		{
+			// current best number of matches (amino acids)
 			int matches = 0;
+			
 			for(int i=0;i<sig.length();i++)
 			{
 				if(sig.charAt(i)==a_sig.charAt(i))
